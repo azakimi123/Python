5d2ac606d9a20cee53a0fc029273cbed005da933
azakimi123 <aaron.zakimi@gmail.com>
2021-02-09 21:49:09 -0700

    different printing options

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,17 @@
-name = input('What is your name?')
-print('Hello', name)
+# getting user input and using variables to print text
+# name = input('What is your name?')
+# print('Hello', name)
 
+#demonstration of splitting up a string on diffent lines
+splitstring = 'this is \na test to see if\nthe string will\nprint on different\nlines'
+print(splitstring)
 
+#tab is \t
+tabstring = 'this\tis\ta\ttabbed'
+print(tabstring)
 
+#tripple double quotes allows you to use quotes in strings and line breaks
+print("""You don't have to 
+do line breaks 
+when using tripple
+quotes""")
