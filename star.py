e695d73ca3668072f8fa8eccc87982c74cd48a43
azakimi123 <aaron.zakimi@gmail.com>
2021-02-13 10:53:24 -0700

    looping turtle

---
+++ star.py
@@ -0,0 +1,28 @@
+import turtle
+import math
+
+dot = turtle.Turtle()
+dot.getscreen().bgcolor("#abf9ff")
+dot.penup()
+dot.goto(-100, 50)
+dot.pendown()
+
+# for i in range(5):
+#   dot.forward(200)
+#   dot.left(216)
+
+dot.speed(10)
+
+def star(turtle, size):
+  if size <= 10:
+    return
+  else:
+    for i in range(5):
+      dot.forward(size)
+      star(turtle, size/2)
+      dot.left(216)
+
+star(dot, 200)
+
+
+turtle.done()
