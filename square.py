f8130415acf5fdd03239bf6732c4157088ffe6ea
azakimi123 <aaron.zakimi@gmail.com>
2021-02-13 10:21:56 -0700

    square turtle

---
+++ square.py
@@ -0,0 +1,21 @@
+import turtle
+
+
+
+crush = turtle.Turtle()
+crush.penup()
+crush.forward(100)
+crush.pendown()
+crush.forward(100)
+crush.left(90)
+crush.forward(100)
+crush.left(90)
+crush.forward(100)
+crush.left(90)
+crush.forward(100)
+crush.left(90)
+
+
+turtle.done()
+
+# input("Press any key to exit ...")
