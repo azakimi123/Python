d72ae46722d4e837a98517f3a8a60eaacdf54ba5
azakimi123 <aaron.zakimi@gmail.com>
2021-03-24 23:06:31 -0600

    sorting practice

---
+++ list_test.py
@@ -0,0 +1,10 @@
+states = ["alaska", "utah", "hawaii", "california", "colorado", "idaho"]
+
+
+#sorts the list 
+# by length 
+states.sort(key=len, reverse=True)
+#default alphabetical order
+# states.sort()
+
+print(states)
