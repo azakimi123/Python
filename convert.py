78928a171e3521c7752f14ebd2a561146aa996dc
azakimi123 <aaron.zakimi@gmail.com>
2021-04-05 22:51:02 -0600

    conversion functions

---
+++ convert.py
@@ -0,0 +1,15 @@
+# Put your code here
+def decimalToRep(int, base):
+  pass
+
+# A main for testing your program
+def main():
+    """Tests the function."""
+    print(decimalToRep(10, 10))
+    print(decimalToRep(10, 8))
+    print(decimalToRep(10, 2))
+    print(decimalToRep(10, 16))
+
+# The entry point for program execution
+if __name__ == "__main__":
+    main()
