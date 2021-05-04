2e92f5d942a818babbab131aa76ca209f0cf28e7
azakimi123 <aaron.zakimi@gmail.com>
2021-05-03 19:37:20 -0600

    color changes

--- bls.py
+++ bls.py
@@ -16,14 +16,37 @@ import pandas as pd
 # print(test)
 
 
+# 1. need to parse the correct data from csv file
+# 2. create a dictionary of the correct data
+# 3. create a function to get the total number of jobs
+
 rows = []
 with open('BLS_report.csv') as f:
-  data = reader(f)
+  reader = csv.DictReader(f)
 
-  for row in data:
-    if row[0] == "Year":
-      print(row)
-    if row[0] == '1961':
-      print(row)
+  for row in reader:
+    print(row['Employment'])
+  # data = reader(f)
+  # found_section = False
+  # header = None
+
+  # for row in data:
+  #   if not found_section:
+  #     if len(row) > 0:
+  #       if row[0] == "Year":
+  #         header = next(data)
+  #         # header = next(data)
+  #         found_section = True
+  #   else:
+  #     break
+
+  # print(header)
+
+  # for row in data:
+  #   # if row[0] == "Year":
+  #   #   print(row)
+  #   if row[0] == '1961':
+  #     for num in row[1:]:
+  #       rows.append(num)
   
   # print(rows)
